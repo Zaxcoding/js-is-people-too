e42b8e28820d1c48d8a4cbefaf083fd50a65ff38
Zaxcoding <zaxcoding@gmail.com>
2015-02-03 20:37:32 -0500

    Added some styling and a fade-in effect

--- shakespeare-says/js-version/shakespeare.js
+++ shakespeare-says/js-version/shakespeare.js
@@ -7,37 +7,37 @@ var IndexToString = [];
 function Setup() {
 	var fileInput = document.getElementById('fileInput');
 
-	var wordArray;
-
-	fileInput.addEventListener('change', function(e) {
-
-	    var file = fileInput.files[0];
-
-		if (file.type.match('text/plain')) {
-			var reader = new FileReader();
-
-			reader.onload = function(e) {
-				wordArray = e.target.result.toLowerCase().match(/\S+/g);
-				for (var i = 0; i < wordArray.length; i++) {
-					var word = wordArray[i];
-
-					if (Dictionary[word] === undefined) {
-						Dictionary[word] = [];
-						IndexToString[IndexToString.length] = word;
-					}
-					if(wordArray[i+1]) {
-						var word2 = wordArray[i+1];
-						Dictionary[word][Dictionary[word].length] = word2;
-					}
-				}
-				DictionaryCreated = true;
-			};
-
-			reader.readAsText(file);
-		}
-	});
+	fileInput.addEventListener('change', FileSelected);
 }
 
+function FileSelected(e) 
+{
+    var file = fileInput.files[0];
+
+	if (file.type.match('text/plain')) {
+		var reader = new FileReader();
+
+		reader.onload = ReadSourceFile;
+		reader.readAsText(file);
+	}
+}
+
+function ReadSourceFile(e) {
+	var wordArray = e.target.result.toLowerCase().match(/\S+/g);
+	for (var i = 0; i < wordArray.length; i++) {
+		var word = wordArray[i];
+
+		if (Dictionary[word] === undefined) {
+			Dictionary[word] = [];
+			IndexToString[IndexToString.length] = word;
+		}
+		if(wordArray[i+1]) {
+			var word2 = wordArray[i+1];
+			Dictionary[word][Dictionary[word].length] = word2;
+		}
+	}
+	DictionaryCreated = true;
+}
 
 function GenerateWords(numWords) {
 	if (!DictionaryCreated) {
@@ -65,7 +65,17 @@ function GenerateWords(numWords) {
 		Answer += " " + curr;
 	}
 
-	document.getElementById('generated-text').innerHTML = Answer;
+	FadeInEffect(Answer, 0);
+}
+
+function FadeInEffect(srcText, charsPrinted) {
+	if (charsPrinted < srcText.length) {
+		document.getElementById('generated-text').innerHTML = srcText.substring(0, charsPrinted + 1);
+		charsPrinted++;
+		setTimeout(function() {
+			FadeInEffect(srcText, charsPrinted)
+		}, 10);
+	}
 }
 
 Setup();
